4f7ce269860e79a91272dec99260972b85d516db
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-05-03 14:13:45 +0430

    height fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,11 +98,9 @@ const Home = () => {
         <title>رزرو بیگزاده</title>
         <meta name="description" content="سایت رزور بلیط هواپیما بیگزاده" />
       </Head>
-      <div className=" bg-no-repeat bg-cover bg-center h-full max-md:h-auto">
+      <div className=" bg-no-repeat bg-cover bg-center max-md:h-auto">
         <div
-          className={`flex justify-center items-center max-md:h-auto ${
-            showFlights ? "h-auto" : "h-1/2"
-          } `}
+          className={`flex justify-center items-center max-md:h-auto`}
         >
           <div className="background bg-right h-[560px] bg-cover absolute w-full top-0 -z-10"></div>
           <div className="space-y-10 p-5 w-[850px] max-md:w-full text-center">
@@ -157,7 +155,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <Contact className={showFlights ? "h-auto" : "h-1/2 max-md:h-auto"} />
+        <Contact className={showFlights ? "h-auto" : "h-[600px] max-md:h-auto"} />
       </div>
     </>
   );
